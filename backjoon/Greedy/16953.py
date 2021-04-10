9d6a5c982032ff5e1d227939316607f3e559a61b
junbeoman <wnsqja8744@gmail.com>
2021-04-10 22:24:28 +0900

    modify 16953 problem in a better way without bfs

--- backjoon/Greedy/16953.py
+++ backjoon/Greedy/16953.py
@@ -1,32 +1,55 @@
+# import sys
+# from collections import deque
+
+# A, B = map(int, sys.stdin.readline().split())
+
+# visited = set()
+# q = []
+# res = -1
+# q = deque(q)
+# q.append((A, 0))
+# visited.add(A)
+
+# while q:
+# 	node, cnt = q.popleft()
+# 	if node > B:
+# 		continue
+
+# 	if node == B:
+# 		res = cnt + 1
+# 		break
+
+# 	x = node * 2
+# 	if x not in visited:
+# 		q.append((x, cnt + 1))
+# 		visited.add(x)
+
+# 	y = int(str(node) + "1")
+# 	if y not in visited:
+# 		q.append((y, cnt + 1))
+# 		visited.add(y)
+
+# print(res)
+
 import sys
-from collections import deque
 
-A, B = map(int, sys.stdin.readline().split())
-
-visited = set()
-q = []
-res = -1
-q = deque(q)
-q.append((A, 0))
-visited.add(A)
-
-while q:
-	node, cnt = q.popleft()
-	if node > B:
-		continue
-
-	if node == B:
-		res = cnt + 1
+a, b = map(int, sys.stdin.readline().split())
+cnt = 0
+while True:
+	if a == b:
+		cnt += 1
+		break
+	if a > b:
+		cnt = -1
 		break
 
-	x = node * 2
-	if x not in visited:
-		q.append((x, cnt + 1))
-		visited.add(x)
+	if b % 10 == 1:
+		b = b // 10
+	elif b % 2 == 0:
+		b /= 2
+	else:
+		cnt = -1
+		break
+	cnt += 1
 
-	y = int(str(node) + "1")
-	if y not in visited:
-		q.append((y, cnt + 1))
-		visited.add(y)
-
-print(res)
+print(cnt)
