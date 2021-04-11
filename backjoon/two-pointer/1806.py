2cc6b32f993710618b79e72d10d63141f8c6ca18
junbeoman <wnsqja8744@gmail.com>
2021-04-12 02:14:21 +0900

    solve two-pointer problem 1806

---
+++ backjoon/two-pointer/1806.py
@@ -0,0 +1,26 @@
+N,S = map(int, input().split())
+nums = list(map(int, input().split()))
+
+s = 0
+e = 0
+acc = 0
+res = 1000000
+
+while True:
+	if acc >= S:
+		if res > e - s:
+			res = e - s
+		acc -= nums[s]
+		s += 1
+	else:
+		if e == N:
+			break
+		acc += nums[e]
+		e += 1
+
+if res == 100000:
+	print(0)
+else:
+	print(res)
+
+
