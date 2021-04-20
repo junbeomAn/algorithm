695d790e8deb70088a5dfc13756094ccfaf5c186
junbeoman <wnsqja8744@gmail.com>
2021-04-20 23:30:30 +0900

    modify boj 10026 with stack structure

--- backjoon/dfs/10026.py
+++ backjoon/dfs/10026.py
@@ -1,7 +1,6 @@
 # visited = True 재귀 visited=False를 해주는 것은 완전탐색과 같은 조합 및 순열 문제에서 썼던듯하다.
 import sys
 input=sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
 N=int(input())
 picture=[list(input().rstrip()) for _ in range(N)]
@@ -16,15 +15,19 @@ def check_range(x,y):
 	return False
 
 def dfs(x,y,color):
-	if picture[x][y] == 'G':
-		picture[x][y]='R'
-	visit[x][y]=1
-	for i in range(4):
-		nx=x+dx[i]
-		ny=y+dy[i]
-		if check_range(nx, ny) and not visit[nx][ny] and color == picture[nx][ny]:
-			visit[nx][ny]=1
-			dfs(nx,ny,color)
+	stack=[]
+	stack.append((x,y))
+	while stack:
+		cx,cy=stack.pop()
+		if picture[cx][cy] == 'G':
+			picture[cx][cy]='R'
+		visit[cx][cy]=1
+		for i in range(4):
+			nx=cx+dx[i]
+			ny=cy+dy[i]
+			if check_range(nx, ny) and not visit[nx][ny] and color == picture[nx][ny]:
+				visit[nx][ny]=1
+				stack.append((nx,ny))
 cnt=0
 for i in range(N):
 	for j in range(N):
